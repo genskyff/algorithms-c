775f34d5d387f124c0529393333ade245864e3ce
Kirisame <gensky377@gmail.com>
2023-08-29 21:32:29 +0900

    fix

--- data-structure/linear_list.c
+++ data-structure/linear_list.c
@@ -11,28 +11,28 @@ typedef struct LinearList
     // 线性表长度
     ElemType data[MAXSIZE];
     // 数据长度
-    int length;
+    size_t len;
 
     void (*clear)(struct LinearList *);
     void (*show)(struct LinearList);
     bool (*is_empty)(struct LinearList);
-    bool (*get_index)(struct LinearList, ElemType, int *);
-    bool (*get)(struct LinearList, int, ElemType *);
-    bool (*insert)(struct LinearList *, int, ElemType);
-    bool (*update)(struct LinearList *, int, ElemType);
-    bool (*remove)(struct LinearList *, int i, ElemType *);
+    bool (*get_index)(struct LinearList, ElemType, size_t *);
+    bool (*get)(struct LinearList, size_t, ElemType *);
+    bool (*insert)(struct LinearList *, size_t, ElemType);
+    bool (*update)(struct LinearList *, size_t, ElemType);
+    bool (*remove)(struct LinearList *, size_t, ElemType *);
 } LinearList;
 
-// 初始化
+// 清空
 void clear(LinearList *L)
 {
-    L->length = 0;
+    L->len = 0;
 }
 
 // 打印
 void show(LinearList L)
 {
-    for (int i = 0; i < L.length; i++)
+    for (size_t i = 0; i < L.len; i++)
         printf("%d ", L.data[i]);
     printf("\n");
 }
@@ -40,16 +40,16 @@ void show(LinearList L)
 // 是否为空
 bool is_empty(LinearList L)
 {
-    return L.length == 0 ? true : false;
+    return L.len == 0 ? true : false;
 }
 
 // 获取值为e的元素下标i
-bool get_index(LinearList L, ElemType e, int *i)
+bool get_index(LinearList L, ElemType e, size_t *i)
 {
-    if (L.length == 0)
+    if (L.len == 0)
         return false;
 
-    for (int j = 0; j < L.length; j++)
+    for (size_t j = 0; j < L.len; j++)
         if (e == L.data[j])
             *i = j;
 
@@ -57,9 +57,9 @@ bool get_index(LinearList L, ElemType e, int *i)
 }
 
 // 获取下标为i的元素值e
-bool get(LinearList L, int i, ElemType *e)
+bool get(LinearList L, size_t i, ElemType *e)
 {
-    if (L.length == 0 || i < 0 || i > L.length - 1)
+    if (L.len == 0 || i < 0 || i > L.len - 1)
         return false;
 
     *e = L.data[i];
@@ -68,25 +68,25 @@ bool get(LinearList L, int i, ElemType *e)
 }
 
 // 下标i处插入值为e的元素
-bool insert(LinearList *L, int i, ElemType e)
+bool insert(LinearList *L, size_t i, ElemType e)
 {
-    if (L->length == MAXSIZE || i < 0 || i > L->length)
+    if (L->len == MAXSIZE || i < 0 || i > L->len)
         return false;
 
-    if (i < L->length)
-        for (int j = L->length - 1; j >= i; j--)
+    if (i < L->len)
+        for (size_t j = L->len - 1; j >= i; j--)
             L->data[j + 1] = L->data[j];
 
     L->data[i] = e;
-    L->length++;
+    L->len++;
 
     return true;
 }
 
 // 修改下标i处的元素值为e
-bool update(LinearList *L, int i, ElemType e)
+bool update(LinearList *L, size_t i, ElemType e)
 {
-    if (L->length == 0 || i < 0 || i > L->length - 1)
+    if (L->len == 0 || i < 0 || i > L->len - 1)
         return false;
 
     L->data[i] = e;
@@ -95,18 +95,18 @@ bool update(LinearList *L, int i, ElemType e)
 }
 
 // 删除下标i处的元素并获取其值
-bool delete(LinearList *L, int i, ElemType *e)
+bool delete(LinearList *L, size_t i, ElemType *e)
 {
-    if (L->length == 0 || i < 0 || i > L->length - 1)
+    if (L->len == 0 || i < 0 || i > L->len - 1)
         return false;
 
     *e = L->data[i];
 
-    if (i < L->length - 1)
-        for (int j = i; j < L->length - 1; j++)
+    if (i < L->len - 1)
+        for (size_t j = i; j < L->len - 1; j++)
             L->data[j] = L->data[j + 1];
 
-    L->length--;
+    L->len--;
 
     return true;
 }
@@ -126,8 +126,8 @@ int main(void)
         update(&L, j, 10);
     show(L);
 
-    while (get_index(L, 10, &i))
-        delete(&L, i, &e);
+    while (get_index(L, 10, i))
+        delete (&L, i, &e);
     show(L);
 
     return 0;
