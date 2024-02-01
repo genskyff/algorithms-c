03b684d310de50f523696925303e8d70ef984611
Kirisame <gensky377@gmail.com>
2024-02-02 02:27:08 +0900

    Remove editor configurations, unused files, and obsolete code

---
+++ test/sort/sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int a[] = {6, 5, 3, 1, 8, 7, 2, 4};
+  int n = sizeof(a) / sizeof(int);
+
+  printf("排序前:\n");
+  display(a, n);
+
+  bubble_sort(a, n);
+
+  printf("排序后:\n");
+  display(a, n);
+
+  return 0;
+}
