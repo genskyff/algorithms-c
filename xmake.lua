90a9a8d1cc269227bd0f609fd6d25e96400fb1b5
Kirisame <gensky377@gmail.com>
2024-02-04 00:05:12 +0900

    add support for test mode and test tasks

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,7 @@
 set_project("algorithms-c")
 set_version("0.1.0")
 set_languages("c17")
+set_allowedplats("windows", "linux", "macosx")
 
 if is_plat("windows") then
     add_toolchains("mingw")
@@ -10,8 +11,42 @@ elseif is_plat("macosx") then
     add_toolchains("clang")
 end
 
-add_rules("mode.debug", "mode.release")
+option("test", { default = false, showmenu = true, description = "Enable test" })
+
+if has_config("test") then
+    add_requires("gtest")
+end
+
+rule("mode.test")
+    on_load(function (target)
+        if not has_config("test") then
+            target:set("enabled", false)
+            return
+        end
+        target:set("rundir", os.projectdir())
+        target:set("group", "test")
+        target:add("packages", "gtest")
+    end)
+rule_end()
+
+task("test")
+    on_run(function ()
+        os.exec("xmake f -m debug --test=y")
+        os.exec("xmake build -g test")
+        os.exec("xmake run -g test")
+    end)
+
+    set_menu{}
+task_end()
+
+add_rules("mode.debug", "mode.release", "mode.test")
 add_includedirs("include", "include/sort")
+
+on_load(function (target)
+    if target:name() ~= "utils" then
+        target:add("deps", "utils")
+    end
+end)
 
 target("utils")
     set_kind("static")
@@ -19,14 +54,15 @@ target("utils")
 
 target("test_utils")
     set_kind("binary")
+    set_group("test")
     add_files("test/utils.c")
 
 target("bubble")
     set_kind("static")
     add_files("src/sort/bubble.c")
-    add_deps("utils")
 
 target("test_bubble")
     set_kind("binary")
+    set_group("test")
     add_files("test/sort/bubble.c")
-    add_deps("bubble", "utils")
+    add_deps("bubble")
