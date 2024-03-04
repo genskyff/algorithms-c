21bf3f22720654798d4ea73b58ada57d5f1ea601
Kirisame <gensky377@gmail.com>
2024-03-04 20:22:34 +0900

    refactor merge sort and quick sort

--- src/sort/merge.c
+++ src/sort/merge.c
@@ -1,63 +1,7 @@
 #include "merge.h"
 #include <stdlib.h>
 
-void msort_recu(elem_t *arr, elem_t *tmp, size_t low, size_t high);
-void msort_iter(elem_t *arr, elem_t *tmp, size_t len);
-void merge(elem_t *arr, elem_t *tmp, size_t low, size_t mid, size_t high);
-
-void merge_sort_recu(elem_t *arr, size_t len) {
-    if (len < 2) {
-        return;
-    }
-
-    elem_t *tmp = (elem_t *)malloc(len * sizeof(elem_t));
-    if (tmp == NULL) {
-        return;
-    }
-
-    msort_recu(arr, tmp, 0, len - 1);
-    free(tmp);
-}
-
-void msort_recu(elem_t *arr, elem_t *tmp, size_t low, size_t high) {
-    if (low < high) {
-        size_t mid = low + (high - low) / 2;
-
-        msort_recu(arr, tmp, low, mid);
-        msort_recu(arr, tmp, mid + 1, high);
-        merge(arr, tmp, low, mid, high);
-    }
-}
-
-void merge_sort_iter(elem_t *arr, size_t len) {
-    if (len < 2) {
-        return;
-    }
-
-    elem_t *tmp = (elem_t *)malloc(len * sizeof(elem_t));
-    if (tmp == NULL) {
-        return;
-    }
-
-    msort_iter(arr, tmp, len);
-    free(tmp);
-}
-
-void msort_iter(elem_t *arr, elem_t *tmp, size_t len) {
-    size_t low, mid, high;
-
-    for (size_t i = 1; i < len; i *= 2) {
-        low = 0;
-        while (low + i < len) {
-            mid  = low + i - 1;
-            high = mid + i < len ? mid + i : len - 1;
-            merge(arr, tmp, low, mid, high);
-            low = high + 1;
-        }
-    }
-}
-
-void merge(elem_t *arr, elem_t *tmp, size_t low, size_t mid, size_t high) {
+void _merge(elem_t *arr, elem_t *tmp, size_t low, size_t mid, size_t high) {
     size_t l_pos = low;
     size_t h_pos = mid + 1;
     size_t t_pos = low;
@@ -81,4 +25,56 @@ void merge(elem_t *arr, elem_t *tmp, size_t low, size_t mid, size_t high) {
     for (size_t i = low; i < t_pos; i++) {
         arr[i] = tmp[i];
     }
+}
+
+void _msort_iter(elem_t *arr, elem_t *tmp, size_t len) {
+    size_t low, mid, high;
+
+    for (size_t i = 1; i < len; i *= 2) {
+        low = 0;
+        while (low + i < len) {
+            mid  = low + i - 1;
+            high = mid + i < len ? mid + i : len - 1;
+            _merge(arr, tmp, low, mid, high);
+            low = high + 1;
+        }
+    }
+}
+
+void _msort_recu(elem_t *arr, elem_t *tmp, size_t low, size_t high) {
+    if (low < high) {
+        size_t mid = low + (high - low) / 2;
+
+        _msort_recu(arr, tmp, low, mid);
+        _msort_recu(arr, tmp, mid + 1, high);
+        _merge(arr, tmp, low, mid, high);
+    }
+}
+
+void merge_sort_recu(elem_t *arr, size_t len) {
+    if (len < 2) {
+        return;
+    }
+
+    elem_t *tmp = (elem_t *)malloc(len * sizeof(elem_t));
+    if (tmp == NULL) {
+        return;
+    }
+
+    _msort_recu(arr, tmp, 0, len - 1);
+    free(tmp);
+}
+
+void merge_sort_iter(elem_t *arr, size_t len) {
+    if (len < 2) {
+        return;
+    }
+
+    elem_t *tmp = (elem_t *)malloc(len * sizeof(elem_t));
+    if (tmp == NULL) {
+        return;
+    }
+
+    _msort_iter(arr, tmp, len);
+    free(tmp);
 }
--- src/sort/quick.c
+++ src/sort/quick.c
@@ -1,37 +1,21 @@
 #include "quick.h"
 #include "utils.h"
 
-void   qsort(elem_t *arr, size_t low, size_t high);
-size_t partition(elem_t *arr, size_t low, size_t high);
-void   move_pivot_to_high(elem_t *arr, size_t low, size_t high);
+void _move_pivot_to_high(elem_t *arr, size_t low, size_t high) {
+    size_t mid   = low + (high - low) / 2;
+    size_t pivot = high;
 
-void quick_sort(elem_t *arr, size_t len) {
-
-    if (len == 0) {
-        return;
+    if ((arr[low] < arr[mid]) ^ (arr[low] < arr[high])) {
+        pivot = low;
+    } else if ((arr[mid] < arr[low]) ^ (arr[mid] < arr[high])) {
+        pivot = mid;
     }
 
-    qsort(arr, 0, len - 1);
+    _swap(arr, pivot, high);
 }
 
-void qsort(elem_t *arr, size_t low, size_t high) {
-    while (low < high) {
-        size_t pivot = partition(arr, low, high);
-
-        if (pivot - low < high - pivot) {
-            if (pivot > 0) {
-                qsort(arr, low, pivot - 1);
-            }
-            low = pivot + 1;
-        } else {
-            qsort(arr, pivot + 1, high);
-            high = pivot - 1;
-        }
-    }
-}
-
-size_t partition(elem_t *arr, size_t low, size_t high) {
-    move_pivot_to_high(arr, low, high);
+size_t _partition(elem_t *arr, size_t low, size_t high) {
+    _move_pivot_to_high(arr, low, high);
     elem_t pivot = arr[high];
     size_t cur   = low;
 
@@ -46,15 +30,27 @@ size_t partition(elem_t *arr, size_t low, size_t high) {
     return cur;
 }
 
-void move_pivot_to_high(elem_t *arr, size_t low, size_t high) {
-    size_t mid   = low + (high - low) / 2;
-    size_t pivot = high;
+void _qsort(elem_t *arr, size_t low, size_t high) {
+    while (low < high) {
+        size_t pivot = _partition(arr, low, high);
 
-    if ((arr[low] < arr[mid]) ^ (arr[low] < arr[high])) {
-        pivot = low;
-    } else if ((arr[mid] < arr[low]) ^ (arr[mid] < arr[high])) {
-        pivot = mid;
+        if (pivot - low < high - pivot) {
+            if (pivot > 0) {
+                _qsort(arr, low, pivot - 1);
+            }
+            low = pivot + 1;
+        } else {
+            _qsort(arr, pivot + 1, high);
+            high = pivot - 1;
+        }
+    }
+}
+
+void quick_sort(elem_t *arr, size_t len) {
+
+    if (len == 0) {
+        return;
     }
 
-    _swap(arr, pivot, high);
+    _qsort(arr, 0, len - 1);
 }
