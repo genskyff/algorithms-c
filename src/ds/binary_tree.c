0fd584bac9956d15cc7f4656831de0b00c0de0be
Kirisame <gensky377@gmail.com>
2024-09-05 12:38:23 +0900

    refactor(binary_tree): use has_alloc_err

--- src/ds/binary_tree.c
+++ src/ds/binary_tree.c
@@ -4,6 +4,24 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+TreeNode *_create_node(elem_t e, TreeNode *left, TreeNode *right,
+                       const char *location);
+
+TreeNode *_create_node(elem_t e, TreeNode *left, TreeNode *right,
+                       const char *location) {
+    const char *_location =
+        (location == NULL || *location == '\0') ? __func__ : location;
+
+    TreeNode *node = (TreeNode *)malloc(sizeof(TreeNode));
+    _has_alloc_err(node, _location);
+
+    node->data  = e;
+    node->left  = left;
+    node->right = right;
+
+    return node;
+}
+
 BinaryTree create(void) {
     BinaryTree tree = {.root = NULL, .vertex_count = 0, .edge_count = 0};
 
@@ -11,17 +29,7 @@ BinaryTree create(void) {
 }
 
 BinaryTree create_root(elem_t e) {
-    TreeNode *root = (TreeNode *)malloc(sizeof(TreeNode));
-    if (root == NULL) {
-        fprintf(stderr,
-                "\x1b[1;31merror: \x1b[0mfailed to allocate memory (exec "
-                "\x1b[33mcreate_root\x1b[0m)\n\n");
-        exit(EXIT_FAILURE);
-    }
-
-    root->data      = e;
-    root->left      = NULL;
-    root->right     = NULL;
+    TreeNode  *root = _create_node(e, NULL, NULL, __func__);
     BinaryTree tree = {.root = root, .vertex_count = 1, .edge_count = 0};
 
     return tree;
